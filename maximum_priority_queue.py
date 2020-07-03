b03f8161d8807660df9bff888b35f0df917da73a
suyi <ysu1996@bupt.edu.cn>
2020-07-03 12:04:20 +0800

    《算法导论》6.5 优先队列

--- maximum_priority_queue.py
+++ maximum_priority_queue.py
@@ -4,7 +4,8 @@ from heap import Heap
 class MaximumPriorityQueue:
     def __init__(self, nums):
         self.heap_instance = Heap(nums)
-        self.heap_instance.heapsort()
+        self.heap_instance.build_max_heap()
+        self.heap_instance.heap_length = self.heap_instance.nums_length
 
     def maximum(self):
         return self.heap_instance.nums[0]
@@ -14,7 +15,7 @@ class MaximumPriorityQueue:
             print("heap underflow")
             return None
         max = self.heap_instance.nums[0]
-        self.heap_instance.nums[0] = self.heap_instance.nums[self.heap_instance.heap_length-1]
+        self.heap_instance.nums[0], self.heap_instance.nums[self.heap_instance.heap_length-1] = self.heap_instance.nums[self.heap_instance.heap_length-1], self.heap_instance.nums[0]
         self.heap_instance.heap_length -= 1
         self.heap_instance.max_heapify(0)
         return max
@@ -25,11 +26,15 @@ class MaximumPriorityQueue:
             return None
 
         self.heap_instance.nums[index] = key
-        while index > 0 and self.heap_instance.nums[self.heap_instance.parent(index)] < self.heap_instance.nums[index]
+        while index > 0 and self.heap_instance.nums[self.heap_instance.parent(index)] < self.heap_instance.nums[index]:
             self.heap_instance.nums[index], self.heap_instance.nums[self.heap_instance.parent(index)] = self.heap_instance.nums[self.heap_instance.parent(index)], self.heap_instance.nums[index]
             index = self.heap_instance.parent(index)
 
     def insert(self, key):
         self.heap_instance.heap_length += 1
-        self.heap_instance.nums[self.heap_instance.heap_length-1] = float('-inf')
+        self.heap_instance.nums_length += 1
+        if self.heap_instance.heap_length == self.heap_instance.nums_length:
+            self.heap_instance.nums.append(float('-inf'))
+        else:
+            self.heap_instance.nums[self.heap_instance.heap_length - 1] = float('-inf')
         self.increase_key(self.heap_instance.heap_length-1, key)
