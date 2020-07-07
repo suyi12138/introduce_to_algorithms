7146b1ad008ae2f12e82d46674abeb7d85d0490c
suyi <ysu1996@bupt.edu.cn>
2020-07-07 11:39:59 +0800

    《算法导论》7.1 快速排序

---
+++ quick_sort.py
@@ -0,0 +1,23 @@
+# 7.1
+
+class QuickSort:
+
+    def __init__(self, nums: list):
+        self.nums = nums
+
+    def partition(self, q: int, r: int):
+        x = self.nums[r]
+        i = q - 1
+        for j in range(q, r):
+            if self.nums[j] <= x:
+                i += 1
+                self.nums[j], self.nums[i] = self.nums[i], self.nums[j]
+
+        self.nums[r], self.nums[i+1] = self.nums[i+1], self.nums[r]
+        return i+1
+
+    def quick_sort(self, p, r):
+        if p < r:
+            q = self.partition(p, r)
+            self.quick_sort(p, q-1)
+            self.quick_sort(q, r)
