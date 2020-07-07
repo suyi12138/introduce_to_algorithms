ec59564b783b82029a749dbec614c8832fb2092f
suyi <ysu1996@bupt.edu.cn>
2020-07-07 15:31:01 +0800

    《算法导论》7.1 快速排序

--- quick_sort.py
+++ quick_sort.py
@@ -1,14 +1,15 @@
 # 7.1
+import random
 
 class QuickSort:
 
     def __init__(self, nums: list):
         self.nums = nums
 
-    def partition(self, q: int, r: int):
+    def partition(self, p: int, r: int):
         x = self.nums[r]
-        i = q - 1
-        for j in range(q, r):
+        i = p - 1
+        for j in range(p, r):
             if self.nums[j] <= x:
                 i += 1
                 self.nums[j], self.nums[i] = self.nums[i], self.nums[j]
@@ -16,8 +17,19 @@ class QuickSort:
         self.nums[r], self.nums[i+1] = self.nums[i+1], self.nums[r]
         return i+1
 
-    def quick_sort(self, p, r):
+    def randomized_partition(self, p: int, r: int):
+        i = random.randint(p, r)
+        self.nums[i], self.nums[r] = self.nums[r], self.nums[i]
+        return self.partition(p, r)
+
+    def quick_sort(self, p: int, r: int):
         if p < r:
             q = self.partition(p, r)
             self.quick_sort(p, q-1)
             self.quick_sort(q+1, r)
+
+    def randomized_quick_sort(self, p: int, r: int):
+        if p < r:
+            q = self.randomized_partition(p, r)
+            self.randomized_quick_sort(p, q-1)
+            self.randomized_quick_sort(q+1, r)
