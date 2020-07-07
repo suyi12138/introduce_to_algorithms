1d9dacc0243e72c3f5fdbc93d82bdb86f1b27701
suyi <ysu1996@bupt.edu.cn>
2020-07-07 21:16:02 +0800

    《算法导论》8.2 基数排序

---
+++ radix_sort.py
@@ -0,0 +1,29 @@
+# 8.3
+
+
+class RadixSort:
+
+    def __init__(self, nums: list, d: int, k: int):
+        # 待排序数组
+        self.nums = nums
+        # 待排序数组中的每个元素的位数
+        self.d = d
+        # 待排序数组中每个元素每一位的可能取值个数
+        self.k = k
+
+    def radix_sort(self):
+        for i in range(self.d-1, -1, -1):
+            c = [0 for _ in range(self.k+1)]
+            b = [0 for _ in range(len(self.nums))]
+            tmp_nums = [int(str(self.nums[j])[i]) for j in range(len(self.nums))]
+            for j in range(len(self.nums)):
+                c[tmp_nums[j]] += 1
+
+            for j in range(1, self.k+1):
+                c[j] += c[j-1]
+
+            for j in range(len(self.nums)-1, -1, -1):
+                b[c[tmp_nums[j]]-1] = self.nums[j]
+                c[tmp_nums[j]] -= 1
+
+            self.nums = b
